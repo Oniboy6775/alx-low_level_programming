b8c8a430dbcbc7695ee2fe58617341382abdad98
Onisabi Abdullahi <onisabiabdullahi@gmail.com>
2022-12-09 02:17:46 -0800

    first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
+//program to print out using puts
 #include <stdio.h>
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle \n");
+  puts("Programming is like building a multilingual puzzle\n");
   return (0);
 }
