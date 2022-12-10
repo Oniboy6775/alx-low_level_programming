2bd9d019c0b24aea68b89eb6be8369d0ade9bf98
Onisabi Abdullahi <onisabiabdullahi@gmail.com>
2022-12-09 19:31:20 -0800

    my first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description- my first c program
+ *
+ *Return: Always 0 (Sucess)
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
