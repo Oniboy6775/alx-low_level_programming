05c5b6b2533272ac1568240cede5cf086e351a88
Onisabi Abdullahi <onisabiabdullahi@gmail.com>
2022-12-17 22:17:54 -0800

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description - script that displays the sixe of data types
+ *Return: Always 0 (sucess)
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+printf("Size of a char: %lu byte(s)\n", sizeof(a));
+printf("Size of a int: %lu byte(s)\n", sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(e));
+return (0);
+}
