faa47c8612889afcd52be240dd648a9818143515
Onisabi Abdullahi <onisabiabdullahi@gmail.com>
2022-12-25 04:20:37 -0800

    print alphabet

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-/*
- * main: entry point
- * description: function to out push some string
- * Return: Always 0 (success)
- */
+/**
+ *  *main - entry point
+ *   *
+ *    *Description - program to output something using prinf function
+ *     *
+ *      *Return: Always 0 (success)
+ *      */
 int main(void)
 {
 printf("_putchar\n");
