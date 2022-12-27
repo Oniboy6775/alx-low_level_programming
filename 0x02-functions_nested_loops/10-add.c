85249a65a628b9e4000a4b2d5b003b0e592f602e
Onisabi Abdullahi <onisabiabdullahi@gmail.com>
2022-12-27 14:20:24 -0800

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - main function
+ * @a: first oprand
+ * @b: second oprand
+ * Description - A function that add 2 numbers
+ * Return: Always the result of sum
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
