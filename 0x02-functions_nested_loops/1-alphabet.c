ec105cdaf62a599f9a741ed2bf8de8da0dc5d635
Onisabi Abdullahi <onisabiabdullahi@gmail.com>
2022-12-27 01:13:37 -0800

    getting it

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,10 @@
-#include "main.h"
 #include <stdio.h>
 /**
  * main - check the code
  * Description - A function that prints the english alphabet
  * Return - Always 0.
 */
-
+void print_alphabet(void);
 void print_alphabet(void)
 {
 char arr_char[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\0'};
