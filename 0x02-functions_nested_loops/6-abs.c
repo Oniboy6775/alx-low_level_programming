4a9a5e1ac2f5a7b0945ebd19e1bb3bb84c5fc41a
Onisabi Abdullahi <onisabiabdullahi@gmail.com>
2022-12-27 14:02:33 -0800

    task 6 completed

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - Returns the absolute value of integer
+ * @num: the number argumnet
+ * Description - Function that returns absolute value of integer
+ * Return: Always 0.
+*/
+int _abs(int num)
+{
+return abs(num);
+}
