a73869121230d515b602aa46a1a81edbade15231
Onisabi Abdullahi <onisabiabdullahi@gmail.com>
2022-12-27 06:26:12 -0800

    error fixed

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 /**
- *  * _putchar -  initialize my code
- *   *@arg1:the cahracter
- *    * Description - external putchar
- *     *
- *      * Return: - Always int.
- *       */
+ * _putchar -  initialize my code
+ *@arg1:the cahracter
+ *Description - external putchar
+ *Return: - Always int.
+ */
 int _putchar(char arg1);
 /**
- *  * print_alphabet - check the code
- *   * _putchar - replace putchar
- *    * Description - A function that prints the english alphabet
- *     * Return: - Always 0.
- *      */
+ *print_alphabet_x10 - check the code
+ *_putchar - replace putchar
+ *Description - A function that prints the english alphabet
+ *Return: - Always 0.
+*/
+
 void print_alphabet_x10(void)
 {
 	char alphabet;
 	int i;
-for (i = 0;i < 10; i++){ 
+
+for (i = 0; i < 10; i++)
+{
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 _putchar(alphabet);
