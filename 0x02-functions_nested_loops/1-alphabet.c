8e1f2ec9960e96ee729456d3c6713ad8b6261177
Onisabi Abdullahi <onisabiabdullahi@gmail.com>
2022-12-27 05:00:10 -0800

    betty wahala

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 /**
+ * _putchar -  initialize my code
+ *@arg1:the cahracter
+ * Description - external putchar
+ *
+ * Return: - Always int.
+ */
+int _putchar(char arg1);
+/**
  * print_alphabet - check the code
  * _putchar - replace putchar
  * Description - A function that prints the english alphabet
- * Return - Always 0.
-*/
-/**
- * _putchar - replace putchar
- * Description - external putchar
- * Return = Always 0.
+ * Return: - Always 0.
  */
-int _putchar(char);
 void print_alphabet(void)
 {
-  char alphabet;
-  for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 _putchar(alphabet);
 }
-  printf("\n");
+printf("\n");
 }
