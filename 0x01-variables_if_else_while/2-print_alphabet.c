b308581f4500b93a3669e113d4ffc99ce3b7675c
Onisabi Abdullahi <onisabiabdullahi@gmail.com>
2022-12-27 15:56:57 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description - Tis is the function that prints alphabet
+ * Return: Always 0
+ */
+int main(void)
+{
+	char start = 'a';
+while (start <= 'z')
+{
+putchar(start);
+start++;
+}
+return (0);
+}
