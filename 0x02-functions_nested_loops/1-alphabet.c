4ac014a8e248b9e32e583cd14bc4bb40cee09807
Onisabi Abdullahi <onisabiabdullahi@gmail.com>
2022-12-27 02:46:07 -0800

    ptototype added to main.h

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,13 +6,14 @@
  * Return - Always 0.
 */
 void print_alphabet(void);
+int _putchar(char);
 void print_alphabet(void)
 {
 char arr_char[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\0'};
 int i;
 for (i = 0; arr_char[i]; i++)
 {
-putchar(arr_char[i]);
+_putchar(arr_char[i]);
 }
-putchar(10);
+_putchar(10);
 }
