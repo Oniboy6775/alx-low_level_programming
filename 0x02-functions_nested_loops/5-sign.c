ae43213cf73765232024e3b496e46a1ff76dda8c
Onisabi Abdullahi <onisabiabdullahi@gmail.com>
2022-12-27 08:57:22 -0800

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * print_sign - let u print sign according to condition
+ * @n: the number argument
+ * Description - Print out +, - or (0)
+ * Return: return 1 if greater, 0 if n is 0 and return -1 if less than
+*/
+int print_sign(int n)
+{
+if (n == 0)
+{
+printf("0");
+return (0);
+}
+else if (n > 0)
+{
+printf("+");
+return (1);
+}
+else
+{
+printf("-");
+return (-1);
+}
+}
