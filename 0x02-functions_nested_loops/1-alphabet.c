c50672a095ab4c43e7c353b9ab29b97003137466
Onisabi Abdullahi <onisabiabdullahi@gmail.com>
2022-12-27 00:33:37 -0800

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,25 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *  * main - check the code
- *   *Description - This is a function that prints alphabet
- *    * Return: Always 0.
- *     */
+ * main - Entry point
+ * Description - A function that prints the english alphabet
+ * Return - Always 0 (success)
+*/
 void print_alphabet(void);
-
 int main(void)
 {
 print_alphabet();
-   return (0);
+return (0);
 }
-
 void print_alphabet(void)
 {
-char arr_char[27]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','\0'};
+char arr_char[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\0'};
 int i;
-for(i=0; arr_char[i]; i++)
+for (i = 0; arr_char[i]; i++)
 {
-printf("%c",arr_char[i]);
+printf("%c", arr_char[i]);
 }
 putchar(10);
 }
