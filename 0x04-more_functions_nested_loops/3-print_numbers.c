6594e1ff11e9833e4933439d3ad672f033f41a9d
Onisabi Abdullahi <onisabiabdullahi@gmail.com>
2022-12-28 08:43:34 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - entry
+ * Descroption - Tho prints 0 -9
+ * Return: return void
+ */
+void print_numbers(void)
+{
+int i =0;
+while (i < 10)
+{
+_putchar(i);
+i++;
+}
+_putchar(10);
+}
