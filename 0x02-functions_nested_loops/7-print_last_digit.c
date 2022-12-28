50633e8755a463773efec74dd129c8db162f483e
Onisabi Abdullahi <onisabiabdullahi@gmail.com>
2022-12-28 03:30:52 -0800

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * print_last_digit - entry point
+ * @num: the number to check its last digit
+ * Description - check the last digit of a number
+ * Return: Always return the last digit
+ */
+int print_last_digit(int num)
+{
+return (abs(num % 10));
+}
