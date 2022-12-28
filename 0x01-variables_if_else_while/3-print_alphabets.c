68619bef17f0566b0e6ca6815be2263bdfe2c6ce
Onisabi Abdullahi <onisabiabdullahi@gmail.com>
2022-12-27 16:08:21 -0800

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,15 @@ int main(void)
 char start_lower = 'a';
 char start_upper = 'A';
 while (start_lower <= 'z')
+{
 putchar(start_lower);
 start_lower++;
+}
 while (start_upper <= 'Z')
+{
 putchar(start_upper);
 start_upper++;
+}
 putchar(10);
 return (0);
 }
