643bd6c3da48496b25a658c51ea0a5337cd6538e
Onisabi Abdullahi <onisabiabdullahi@gmail.com>
2022-12-28 08:25:40 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - entry
+ * @a: first operand
+ * @b: second oprand
+ * Description - multiplies two number
+ * Return: Always return result
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
