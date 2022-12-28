e7c2091c14601915cdb20c0131976be06ead706d
Onisabi Abdullahi <onisabiabdullahi@gmail.com>
2022-12-28 02:29:46 -0800

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Description - printing alphabet as tebahpla
+ * Return: Always 0
+ */
+int main(void)
+{
+	char i = 'z';
+while (i >= 'a')
+{
+putchar(i);
+i--;
+}
+putchar(10);
+return (0);
+}
