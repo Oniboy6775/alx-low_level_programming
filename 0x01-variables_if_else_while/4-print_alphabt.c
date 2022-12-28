2970c0d16c1323763060ee870d952947a454fc57
Onisabi Abdullahi <onisabiabdullahi@gmail.com>
2022-12-27 16:18:06 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Description - This is my descriptiom
+ * Return: always 0
+ */
+int main(void)
+{	char alpha;
+for (alpha = 'a'; alpha < 'z'; alpha++)
+{
+if (alpha == 'q' || alpha == 'e')
+putchar(0);
+else
+putchar(alpha);
+}
+printf("\n");
+return (0);
+}
