5fb9c9fcc9ed9e02b60b80c8a979d99f3c0462b2
Onisabi Abdullahi <onisabiabdullahi@gmail.com>
2022-12-27 16:05:55 -0800

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Description - print alphabet in lower and upper case
+ * Return: Always 0
+ */
+int main(void)
+{
+char start_lower = 'a';
+char start_upper = 'A';
+while (start_lower <= 'z')
+putchar(start_lower);
+start_lower++;
+while (start_upper <= 'Z')
+putchar(start_upper);
+start_upper++;
+putchar(10);
+return (0);
+}
