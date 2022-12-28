3ad604d27721185c77d745a99b3db7e51d319426
Onisabi Abdullahi <onisabiabdullahi@gmail.com>
2022-12-28 02:23:05 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Description - this print 0-9
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 48;
+while (i <= 57)
+{
+putchar(i);
+i++;
+}
+putchar(10);
+return (0);
+}
