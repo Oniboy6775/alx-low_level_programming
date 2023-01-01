b7f3e55eff0fd66c0ae9e54ca14a4f44e99bca71
Onisabi Abdullahi <onisabiabdullahi@gmail.com>
2023-01-01 02:03:40 -0800

    task complted

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -22,8 +22,11 @@ void print_triangle(int size)
 				for (spaces = size - row; spaces >= 1; spaces--)
 				{											_putchar(' ');
 				}
-				for (hashes = 1; hashes <= row; hashes++)						_putchar('#');
+				for (hashes = 1; hashes <= row; hashes++)
+				{
+					_putchar('#');
+				}
+				_putchar('\n');
 			}
 		}
-		_putchar('\n');
 }								
